623d1aa0943d606efcf9ca53540da4599456e755
unknown <shamveelkhilji@gmail.com>
2026-02-01 15:03:10 +0500

    fixed title

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,14 @@ import { SocialMediaSection } from "@/components/SocialMediaSection"
 import { FAQSection } from "@/components/FAQSection"
 import { ContactSection } from "@/components/ContactSection"
 import { Footer } from "@/components/Footer"
+import { title } from "node:process"
 
 export default function HomePage() {
   return (
     <div className="min-h-screen bg-[#0B0F12] text-white">
+      <title>
+        AlpineLift Charters
+      </title>
       <HeroSection />
       <CTAHelicopterSection />
       <TourSelectionSection />
